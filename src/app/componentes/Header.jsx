969a06d4f4c23f61912d17f328005c32254bfd1a
NEVES58 <joaopedroneves987@gmail.com>
2024-04-14 21:26:34 +0100

    header add

--- src/app/componentes/Header.jsx
+++ src/app/componentes/Header.jsx
@@ -2,22 +2,18 @@ import React from 'react'
 
 function Header() {
     return (
-      <div className='flex items-center justify-between px-20 py-7 bg-gray-800 text-white w-full'>
-        <h1 className="text-lg font-bold">JN</h1>
+      <div className='flex items-center justify-between px-20 py-7 bg-gray-800 text-white w-full text-xl'>
+        <h1 className="text-3xl font-bold text-blue-400">JN</h1>
         <nav>
           <ul className='flex gap-6'>
-            <li>About</li>
-            <li>CV</li>
-            <li>Skills</li>
-            <li>Projects</li>
+            <li><a className='hover:text-blue-400'>About</a></li>
+            <li><a className='hover:text-blue-400'>CV</a></li>
+            <li><a className='hover:text-blue-400'>Skills</a></li>
+            <li><a className='hover:text-blue-400'>Projects</a></li>
           </ul>
         </nav>
       </div>
     );
   }
   
-  
-  
-  
-
 export default Header
